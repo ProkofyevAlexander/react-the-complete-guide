e16d6e04246c40e02a3a15b91b3cd357aed5f12b
Alexander Prokofyev <a.a.prokofyev@mail.ru>
2018-02-03 09:46:55 +0300

    add check for ingredients length equal to zero

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const ingredients = Object
+    let ingredients = Object
         .keys(props.ingredients)
         .map((igKey) => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => (
                    <BurgerIngredient key={igKey + i} type={igKey}/>
                 ))
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if (ingredients.length === 0) {
+        ingredients = <p>Please start adding ingredients</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
