5bd1a865342a4ee59a73ad8e79f5d98343be1d48
Alexander Prokofyev <a.a.prokofyev@mail.ru>
2018-02-04 17:37:05 +0300

    add navigation

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Container from '../../../hoc/Conteiner';
+
+const sideDrawer = (props) => {
+    const attachedClasses = [
+        classes.SideDrawer,
+        props.open
+            ? classes.Open
+            : classes.Close
+    ]
+        .join(' ');
+    return (
+        <Container>
+            <Backdrop
+                show={props.open}
+                clicked={props.closed}/>
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Container>
+    );
+};
+
+export default sideDrawer;
