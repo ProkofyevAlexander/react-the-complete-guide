186dbc7f550d258175ebe0fee2701e51e30892f3
Alexander Prokofyev <a.a.prokofyev@mail.ru>
2018-02-04 18:18:55 +0300

    optimize Modal component update (rerender)

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Container from '../../../hoc/Conteiner';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Container>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show
-                    ? 'translateY(0)'
-                    : 'translateY(-100vh)',
-                opacity: props.show
-                    ? '1'
-                    : '0'
-            }}>
-            {props.children}
-        </div>
-    </Container>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <Container>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show
+                            ? 'translateY(0)'
+                            : 'translateY(-100vh)',
+                        opacity: this.props.show
+                            ? '1'
+                            : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Container>
+        );
+    }
+}
+
+export default Modal;
