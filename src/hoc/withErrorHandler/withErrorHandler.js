bad94b8e4ab09bdaaed4ea79e73317a46002f2a9
Alexander Prokofyev <a.a.prokofyev@mail.ru>
2018-02-16 21:46:51 +0300

    add remove of old axios.interceptors in the withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,18 +9,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
         };
 
         componentWillMount() {
-            axios.interceptors.request
+            this.reqInterceptor = axios.interceptors.request
                 .use((req) => {
                     this.setState({error: null});
                     return req;
                 });
-            axios.interceptors.response
+            this.respInterceptor = axios.interceptors.response
                 .use(
                     (resp) => resp,
                     (error) => {
                         this.setState({error: error});
                     }
                 );
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.respInterceptor);
         }
 
         errorConfirmedHandler = () => {
