d8c4d03d28b6bd51c0f47f933fd65e9d89cd4c2d
Alexander Prokofyev <a.a.prokofyev@mail.ru>
2018-03-25 18:47:27 +0300

    save auth info into localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -18,9 +18,14 @@ export const authFail = (error) => ({
     error: error
 });
 
-export const logout = () => ({
-    type: actionTypes.AUTH_LOGOUT
-});
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
 
 export const checkAuthTimeout = (expirationTime) =>
     (dispatch) => setTimeout(
@@ -44,6 +49,12 @@ export const auth = (email, password, isSignup) =>
         axios
             .post(url, authData)
             .then((response) => {
+                localStorage.setItem('token', response.data.idToken);
+                const expirationDate = new Date(
+                    new Date().getTime() + response.data.expiresIn * 1000
+                );
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(
                     response.data.idToken,
                     response.data.localId
@@ -57,3 +68,23 @@ export const setAuthRedirectPath = (path) => ({
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
 });
+
+export const authCheckState = () =>
+    (dispatch) => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        }
+        else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            const seconds = (expirationDate.getTime() - new Date().getTime()) / 1000;
+            if (seconds > 0) {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(seconds))
+            }
+            else {
+                dispatch(logout());
+            }
+        }
+    };
